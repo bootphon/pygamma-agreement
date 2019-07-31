3bc47095014ee3888da963ad349d20d589f81c27
Rachine <riadrachid3@gmail.com>
2019-07-31 15:11:52 +0200

    start best aligner: set of unitary alignements done

--- pygamma/alignement.py
+++ pygamma/alignement.py
@@ -36,6 +36,7 @@ import numpy as np
 from scipy.special import binom
 
 from matplotlib import pyplot as plt
+from itertools import product
 
 
 class Error(Exception):
@@ -145,6 +146,76 @@ class Alignement(object):
                 elif found > 1:
                     raise SetPartitionError('{} {} assigned twice'.format(
                         annotator, unit))
+
+    @property
+    def disorder(self):
+        """Compute the disorder for the unitary alignement
+        >>> unitary_alignement.compute_disorder() = ...
+        Based on formula (6) of the original paper
+        Note:
+        unit is the equivalent of segment in pyannote
+        """
+        disorder = 0.0
+        for unitary_alignement in self.set_unitary_alignements:
+            disorder += unitary_alignement.disorder
+        return disorder
+
+
+class Best_Alignement(object):
+    """Alignement
+    Parameters
+    ----------
+    continuum :
+        Continuum where the unitary alignement is from
+    combined_dissimilarity :
+        combined_dissimilarity
+    """
+
+    def __init__(
+            self,
+            continuum,
+            combined_dissimilarity,
+    ):
+
+        super(Alignement, self).__init__()
+        self.continuum = continuum
+        self.set_unitary_alignements = self.get_unitary_alignements_best()
+        self.combined_dissimilarity = combined_dissimilarity
+
+        # set partition tests for the unitary alignements
+        for annotator in self.continuum.iterannotators():
+            for unit in self.continuum[annotator].itersegments():
+                found = 0
+                for unitary_alignement in self.set_unitary_alignements:
+                    if [annotator, unit] in unitary_alignement.n_tuple:
+                        found += 1
+                if found == 0:
+                    raise SetPartitionError(
+                        '{} {} not in the set of unitary alignements'.format(
+                            annotator, unit))
+                elif found > 1:
+                    raise SetPartitionError('{} {} assigned twice'.format(
+                        annotator, unit))
+
+    def get_unitary_alignements_best(self):
+        set_of_possible_segments = []
+        for annotator in continuum.iterannotators():
+            set_of_possible_segments.append(
+                [[el, annotator]
+                 for el in list(continuum[annotator].itersegments()) + [None]])
+
+        set_of_possible_tuples = list(product(*set_of_possible_segments))
+        # Property section 5.1.1 to reduce initial complexity
+        set_of_possible_unitary_alignements = []
+        for n_tuple in set_of_possible_tuples:
+            unitary_alignement = Unitary_Alignement(
+                self.continuum, n_tuple, self.combined_dissimilarity)
+
+            # Property section 5.1.1 to reduce initial complexity
+            disorder = unitary_alignement.disorder
+            if disorder < self.combined_dissimilarity.DELTA_EMPTY:
+                set_of_possible_unitary_alignements.append(unitary_alignement)
+        return set_of_possible_unitary_alignements
 
     @property
     def disorder(self):
