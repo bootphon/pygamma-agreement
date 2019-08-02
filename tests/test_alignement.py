0ef3d76e3ae2fe9d5d4adb6e28454b9c6a36d857
Rachine <riadrachid3@gmail.com>
2019-08-02 14:34:05 +0200

    New test for sequence

--- tests/test_alignement.py
+++ tests/test_alignement.py
@@ -6,6 +6,7 @@ from pygamma.continuum import Continuum
 from pygamma.dissimilarity import Categorical_Dissimilarity
 from pygamma.dissimilarity import Positional_Dissimilarity
 from pygamma.dissimilarity import Combined_Categorical_Dissimilarity
+from pygamma.dissimilarity import Combined_Sequence_Dissimilarity
 from pygamma.alignement import Unitary_Alignement, Alignement, Best_Alignement
 
 from pygamma.alignement import SetPartitionError
@@ -277,4 +278,72 @@ def test_best_alignement():
 
     assert best_alignement.disorder == pytest.approx(0.32092361020850013,
                                                      0.001)
+    assert best_alignement.disorder < alignement.disorder
+
+
+def test_best_alignement_sequence():
+    continuum = Continuum()
+    annotation = Annotation()
+    annotation[Segment(1, 5)] = ('a', 'c', 'd')
+    annotation[Segment(6, 8)] = ('a', 'b', 'b', 'd')
+    annotation[Segment(12, 18)] = ('a', 'b', 'c')
+    annotation[Segment(7, 20)] = ('a')
+    continuum['liza'] = annotation
+    annotation = Annotation()
+    annotation[Segment(2, 6)] = ('a', 'b')
+    annotation[Segment(7, 8)] = ('a', 'b', 'c')
+    annotation[Segment(12, 18)] = ('a', 'b', 'b', 'b', 'b')
+    annotation[Segment(8, 10)] = ('a')
+    annotation[Segment(7, 19)] = ('a', 'b', 'b')
+    continuum['pierrot'] = annotation
+    annotation = Annotation()
+
+    annotation[Segment(1, 6)] = ('a', 'b')
+    annotation[Segment(8, 10)] = ('a', 'b', 'b')
+    annotation[Segment(7, 19)] = ('a', )
+    annotation[Segment(19, 20)] = ('a', 'b', 'c', 'c', 'd')
+
+    continuum['hadrien'] = annotation
+
+    symbols = ['a', 'b', 'c', 'd']
+    cat = np.array([[0, 0.5, 0.3, 0.7], [0.5, 0., 0.6, 0.4],
+                    [0.3, 0.6, 0., 0.7], [0.7, 0.4, 0.7, 0.]])
+
+    combi_dis = Combined_Sequence_Dissimilarity(
+        'SR',
+        list_admitted_symbols=symbols,
+        DELTA_EMPTY=0.5,
+        symbol_dissimlarity_matrix=cat)
+
+    set_unitary_alignements = []
+
+    n_tuple = (['liza', Segment(1, 5)], ['pierrot', Segment(2, 6)],
+               ['hadrien', Segment(1, 6)])
+    unitary_alignement = Unitary_Alignement(continuum, n_tuple, combi_dis)
+    set_unitary_alignements.append(unitary_alignement)
+
+    n_tuple = (['liza', Segment(6, 8)], ['pierrot', Segment(7, 8)],
+               ['hadrien', Segment(8, 10)])
+    unitary_alignement = Unitary_Alignement(continuum, n_tuple, combi_dis)
+    set_unitary_alignements.append(unitary_alignement)
+
+    n_tuple = (['liza', Segment(7, 20)], ['pierrot', Segment(7, 19)],
+               ['hadrien', Segment(7, 19)])
+    unitary_alignement = Unitary_Alignement(continuum, n_tuple, combi_dis)
+    set_unitary_alignements.append(unitary_alignement)
+
+    n_tuple = (['liza', Segment(12, 18)], ['pierrot',
+                                           Segment(12, 18)], ['hadrien', None])
+    unitary_alignement = Unitary_Alignement(continuum, n_tuple, combi_dis)
+    set_unitary_alignements.append(unitary_alignement)
+
+    n_tuple = (['liza', None], ['pierrot', Segment(8, 10)],
+               ['hadrien', Segment(19, 20)])
+    unitary_alignement = Unitary_Alignement(continuum, n_tuple, combi_dis)
+    set_unitary_alignements.append(unitary_alignement)
+
+    alignement = Alignement(continuum, set_unitary_alignements, combi_dis)
+
+    best_alignement = Best_Alignement(continuum, combi_dis)
+    assert best_alignement.disorder == pytest.approx(0.4242776023799833)
     assert best_alignement.disorder < alignement.disorder
