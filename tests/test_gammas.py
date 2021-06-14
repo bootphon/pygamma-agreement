20b1bb0f221aaea94b3ca1e7635062eea3412a88
lfavre <favreleopold@gmail.com>
2021-06-14 16:49:54 +0200

    added gamma-cat and gamma-k to the tests

--- tests/test_gammas.py
+++ tests/test_gammas.py
@@ -1,6 +1,7 @@
 """Tests for the gamma computations"""
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 from pygamma_agreement.continuum import Continuum
@@ -16,8 +17,16 @@ def test_gamma_2by1000():
 
     gamma_results = continuum.compute_gamma(dissim, precision_level=0.5)
     assert len(gamma_results.best_alignment.unitary_alignments) == 1085
-    assert 0.38 <= gamma_results.gamma <= 0.41
 
+    # Gamma:
+    assert 0.38 <= gamma_results.gamma <= 0.41
+    # Gamma-cat:
+    assert 0.38 <= gamma_results.gamma_cat <= 0.42
+    # Gamma_k's
+    assert 0.21 <= gamma_results.gamma_k('Adj') <= 0.25
+    assert 0.33 <= gamma_results.gamma_k('Noun') <= 0.37
+    assert 0.31 <= gamma_results.gamma_k('Prep') <= 0.35
+    assert 0.11 <= gamma_results.gamma_k('Verb') <= 0.15
 
 def test_gamma_3by100():
     continuum = Continuum.from_csv(Path("tests/data/3by100.csv"))
@@ -28,7 +37,15 @@ def test_gamma_3by100():
 
     gamma_results = continuum.compute_gamma(dissim, precision_level=0.5)
     assert len(gamma_results.best_alignment.unitary_alignments) == 127
+    # Gamma
     assert 0.79 <= gamma_results.gamma <= 0.81
+    # Gamma-cat:
+    assert 0.96 <= gamma_results.gamma_cat
+    # Gamma_k's
+    assert 0.96 <= gamma_results.gamma_k('Adj')
+    assert 0.94 <= gamma_results.gamma_k('Noun')
+    assert 0.86 <= gamma_results.gamma_k('Verb') <= 0.90
+    assert 0.96 <= gamma_results.gamma_k('Prep')
 
 
 def test_gamma_alexpaulsuzan():
@@ -40,5 +57,14 @@ def test_gamma_alexpaulsuzan():
 
     gamma_results = continuum.compute_gamma(dissim, precision_level=0.01)
     assert len(gamma_results.best_alignment.unitary_alignments) == 7
+
     assert gamma_results.best_alignment.disorder == pytest.approx(0.96, 0.01)
+    # Gamma:
     assert 0.44 <= gamma_results.gamma <= 0.47
+    # Gamma-cat:
+    assert 0.67 <= gamma_results.gamma_cat <= 0.69
+    # Gamma-k's
+    gamma_ks = {'1': 1, '2': 0, '3': 0, '4': 0, '5': 1, '6': 1}
+    for i in range(1, 7):
+        assert gamma_ks[str(i)] == gamma_results.gamma_k(str(i))
+    # assert gamma_results.gamma_k('7') is np.NaN
